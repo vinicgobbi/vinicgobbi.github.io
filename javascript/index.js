beccc3a611194ce6311ec133d3c6cf497d5764b7
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2023-07-27 22:31:35 -0300

    Adicionado 2 projetos ao portfólio

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,7 @@
-function redirect(arg) {
-    if (arg == "home"){
-        window.location.href = '/'
-    }else if (arg == "links"){
-        window.location.href = '/links'
-    }else if (arg == "about"){
-        window.location.href = '/about'
-    }else{
-        window.location.href = '/'
+function port_redirect(type, url) {
+    if (type == "localSite"){
+        window.location.href = url
+    }else if (type == "github"){
+        open(`https://github.com/vinicgobbi/${url}`)
     }
 }
